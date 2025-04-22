61fe3e2811491f99b11b34bad1069a25ae5c53bd
Binada-SH <binadasasmika@gmail.com>
2025-04-22 19:38:26 +0530

    Fade On Scroll Animation Adding To Skill Section

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('.header');
     const socialIcons = document.querySelector('.social_icons');
     const copyrights = document.querySelector('.copyrights');
+    const skill = document.querySelector('.skills_section');
     
     if (scrollPosition > 50) {
       header.style.opacity = 1 - (scrollPosition / 500);
@@ -48,5 +49,11 @@ document.addEventListener('DOMContentLoaded', function() {
       copyrights.style.opacity = 1 - ((scrollPosition - 150) / 400);
     } else {
       copyrights.style.opacity = 1;
+    }
+
+    if (scrollPosition > 150) {
+      skill.style.opacity = 1 - ((scrollPosition - 200) / 80000);
+    } else {
+      skill.style.opacity = 1;
     }
   });
