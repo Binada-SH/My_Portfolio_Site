fc3b52d7ebcecb13eeaef76380827d56a43c830e
Binada-SH <binadasasmika@gmail.com>
2025-05-04 19:07:21 +0530

    NavBar Highlighting

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { // Adjust the scroll threshold as needed
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+});
 document.addEventListener('DOMContentLoaded', function() {
     const mobileMenu = document.getElementById('mobile-menu');
     const navMenu = document.querySelector('.nav-menu');
